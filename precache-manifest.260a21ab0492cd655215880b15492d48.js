dc55f5f600318aad4d5f7c2d7af338c24c9c9e2f
prekar <preetamkamal@gmail.com>
2020-05-07 13:16:36 +0530

    Updates

---
+++ precache-manifest.260a21ab0492cd655215880b15492d48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a42961e4d00bd67f525e0d44702b40",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "a9c6243d9dfcbeef35a6",
+    "url": "/Covid19-Tracker/static/css/main.af826243.chunk.css"
+  },
+  {
+    "revision": "a05ad254df5c93e1442d",
+    "url": "/Covid19-Tracker/static/js/2.83802a67.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-Tracker/static/js/2.83802a67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c6243d9dfcbeef35a6",
+    "url": "/Covid19-Tracker/static/js/main.562f59e0.chunk.js"
+  },
+  {
+    "revision": "3f4d62a98a79cca93490",
+    "url": "/Covid19-Tracker/static/js/runtime-main.ae433cf9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-Tracker/static/media/image.d7265326.png"
+  }
+]);
